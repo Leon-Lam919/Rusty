74de40e68d099e790b3825df195454c24d6b19f8
Leon Lam <leonlam34@gmail.com>
2023-12-20 13:41:35 -0600

    arr

--- test.rs
+++ test.rs
@@ -4,6 +4,10 @@ fn add(one: i32, two: i32) -> i32{
 
 
 fn main(){
-    let mut x = 5;
+    let x: i32 = 5;
+    let alpha_a: char = 'a';
+    let alphabet: [char; 26] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     println!("The value of x is {x}.");
+    println!("The letter of alpha_a is {alpha_a}.");
+    println!("letter 3 of alphabet is {}.", alphabet[2]);
 }
