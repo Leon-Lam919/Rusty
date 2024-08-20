f27ebbbe454e27d150d1590c420d28eda8bc8896
Leon Lam <leonlam34@gmail.com>
2024-08-20 16:44:32 -0500

    add can write to a TODO file that will server as mem

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,6 @@
+use std::io::Write;
+use std::fs::OpenOptions;
+
 fn main(){
     let args: Vec<String> = std::env::args().collect();
 
@@ -8,7 +11,6 @@ fn main(){
 
     let request = &args[1];
     let item = if args.len() > 2 { Some(&args[2]) } else { None };
-    let mut list: Vec<String> = Vec::new();
 
 
     //TODO: Add "list" command to list all todo items
@@ -16,14 +18,16 @@ fn main(){
     //TODO: Add "edit" command to edit a todo item
     if request == "add"{
         if let Some(item) = item {
-            list.push(item.to_string());
+            let mut file = OpenOptions::new()
+                .write(true)
+                .append(true)
+                .create(true)
+                .open("todo.txt")
+                .expect("Failed to open file");
+            writeln!(file, "{}", item).expect("failed to write to file");
             println!("Adding a new todo item");
         } else {
             println!("No item to add");
-        }
-    } else if request == "list"{
-        for (index, item) in list.iter().enumerate(){
-            println!("{}: {}", index, item);
         }
     } else if request == "edit"{
         //TODO: Add edit functionality
