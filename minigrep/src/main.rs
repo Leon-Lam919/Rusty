81c0d14cf47e2bec2708bb55dbabedd2dc520afe
Leon Lam <leonlam34@gmail.com>
2024-08-22 14:38:36 -0500

    Got list implemented

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,21 +1,36 @@
+//TODO: Create CLI for TODO app
+
 use std::io::Write;
 use std::fs::OpenOptions;
+use std::io::BufRead;
 
 fn main(){
     let args: Vec<String> = std::env::args().collect();
 
-    if args.len() < 2{
-        println!("not enough arguments");
-        std::process::exit(1);
-    }
-
     let request = &args[1];
     let item = if args.len() > 2 { Some(&args[2]) } else { None };
 
+    // *Will list all todo items
+    if request == "list"{
+        let file = OpenOptions::new()
+            .read(true)
+            .open("todo.txt")
+            .expect("Failed to open file");
+        let reader = std::io::BufReader::new(file);
+        
+        for (index, line) in reader.lines().enumerate(){
+            println!("{}: {}", index + 1, line.unwrap());
+        }
+        println!("not enough arguments");
+        std::process::exit(0);
+    }
 
-    //TODO: Add "list" command to list all todo items
-    //TODO: Add "done" command to mark a todo item as done
-    //TODO: Add "edit" command to edit a todo item
+    if args.len() < 2 {
+        println!("not enough arguments");
+        std::process::exit(0);
+    }
+
+    //* add a new todo item
     if request == "add"{
         if let Some(item) = item {
             let mut file = OpenOptions::new()
@@ -29,12 +44,17 @@ fn main(){
         } else {
             println!("No item to add");
         }
-    } else if request == "edit"{
-        //TODO: Add edit functionality
+    }
+    //TODO: Mark items as complete
+    else if request == ""{
+    }
+    //* edit list
+    else if request == "edit"{
+
+    //TODO: Add edit functionality
     } else {
         println!("Invalid command");
     }
 
-    //TODO: Create CLI for TODO app
 
 }
