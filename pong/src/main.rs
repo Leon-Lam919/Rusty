623061a84ca895dea41940d4ec4994af1860a144
Leon Lam <leonlam34@gmail.com>
2024-09-01 14:56:31 -0500

    creating pong

--- pong/src/main.rs
+++ pong/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use ggez::*;
+
+struct State {}
+
+impl ggez::event::EventHandler<GameError> for State {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+}
+
+pub fn main(){
+
 }
