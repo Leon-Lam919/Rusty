49138ee1d300b3caaa467b2bfd8d90a508f503e4
Leon Lam <leonlam34@gmail.com>
2024-09-06 10:42:38 -0500

    Added pseudocode for HTTP

--- HTTP_server/src/main.rs
+++ HTTP_server/src/main.rs
@@ -1,8 +1,19 @@
-// import crates that help with HTTP requests
+// add library, actix_web
+
+// !request handler
+// * Function handles_get_request: takes a request and returns a response
+// * Function handles_post_request: takes a request and returns a response
+
+// !initalize server function
+// * Function initalize_server: takes a port number and returns a server
 
 
-// will be used to run the server
+// !route request
+//*  define route for GET request
+//*  define route for POST request
+
+// !main function
+// * call initalize_server function
+// * listen for incoming connections
 fn main() {
 }
-
-// Request handler
