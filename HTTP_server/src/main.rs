361a0f685d8db7903981ffa044b5843be407e93f
Leon Lam <leonlam34@gmail.com>
2024-09-10 21:55:38 -0500

    gitignore

--- HTTP_server/src/main.rs
+++ HTTP_server/src/main.rs
@@ -1,4 +1,8 @@
-use std::net::TcpListener;
+
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream}
+};
 
 // func request handler
 // @ Function handles_get_request: takes a request and returns a response
@@ -17,4 +21,19 @@ use std::net::TcpListener;
 
 fn main(){
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request:Vec<_> = buf_reader
+    .lines()
+    .map(|result| result.unwrap())
+    .take_while(|line| !line.is_empty())
+    .collect();
+
+    println!("Request: {http_request:#?}");
 }
