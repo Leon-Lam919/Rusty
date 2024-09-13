8aba893ca832728e88e990d6d6cae10f732e77e7
Leon Lam <leonlam34@gmail.com>
2024-09-13 08:13:00 -0500

    Added a html file

--- HTTP_server/src/main.rs
+++ HTTP_server/src/main.rs
@@ -1,11 +1,32 @@
-
 use std::{
+    fs,
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}
+    net::{TcpListener, TcpStream},
 };
 
+
 // func request handler
-// @ Function handles_get_request: takes a request and returns a response
+// @ Function handle_connection: takes a request and returns a response
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let status_line = "HTTP/1.1 200 OK";
+    let content = fs::read_to_string("hello.html").unwrap();
+    let len = content.len();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line, len, content
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+}
 
 // func initalize server function
 // @ Function initalize_server: takes a port number and returns a server
@@ -19,21 +40,12 @@ use std::{
 // @ call initalize_server function
 // @ listen for incoming connections
 
-fn main(){
+fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    for stream in listener.incoming(){
+
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
+
         handle_connection(stream);
     }
-}
-
-fn handle_connection(mut stream: TcpStream){
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request:Vec<_> = buf_reader
-    .lines()
-    .map(|result| result.unwrap())
-    .take_while(|line| !line.is_empty())
-    .collect();
-
-    println!("Request: {http_request:#?}");
 }
