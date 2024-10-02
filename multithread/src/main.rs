e4fcb9eb666768624f7ffb4e46097d064fba235b
Leon Lam <leonlam34@gmail.com>
2024-10-02 16:24:38 -0500

    added mutex

--- multithread/src/main.rs
+++ multithread/src/main.rs
@@ -2,13 +2,18 @@ use std::thread;
 use std::sync::{Mutex, Arc};
 
 // max number of guest
-const guest: usize = 25;
+const GUEST: usize = 25;
+
 
 //Creates threads given the number of guest ~25~
 pub fn threads(guest_count: usize){
+    let counter = Arc::new(Mutex::new(0));
+
     let thrd = thread::spawn(move||{
         for i in 1..guest_count+1{
-            println!("Hi from {i} thread count.")
+            let counter = Arc::clone(&counter);
+            let mut mutex = counter.lock().unwrap();
+        
         }
     });
     // Thread returns back and prints which thread it is
