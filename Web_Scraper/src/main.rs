7810cdf42dc26e384b70f9866640982c92d809bb
Leon Lam <leonlam34@gmail.com>
2024-10-02 21:04:43 -0500

    added comments

--- Web_Scraper/src/main.rs
+++ Web_Scraper/src/main.rs
@@ -5,12 +5,15 @@ use spider::website::Website;
 
 #[tokio::main]
 async fn main() {
+    // website to scrape
     let mut website: Website = Website::new("https://manganato.com");
 
+    // start timer and set web crawler to go
     let start = Instant::now();
     website.crawl().await;
     let duration = start.elapsed();
 
+    // get all the links from the site
     let links = website.get_links();
 
     for link in links.iter(){
